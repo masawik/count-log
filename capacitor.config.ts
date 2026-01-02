d2a15a09162711e9527b3364c63073a33cfe880e
Aleksandr <rocheval.dev@gmail.com>
2026-01-02 23:17:02 +0300

    build: set up capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.countlog.app',
+  appName: 'CountLog',
+  webDir: 'build/client',
+}
+
+export default config
