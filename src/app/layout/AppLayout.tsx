8e60a2407a18daeda6bf30d23a97352d0d567376
Aleksandr <rocheval.dev@gmail.com>
2026-01-02 23:25:24 +0300

    style: eslint cleanup

--- src/app/layout/AppLayout.tsx
+++ src/app/layout/AppLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 export default function AppLayout() {
   return (
     <div>
-      <main className='container mx-auto px-2'>
+      <main className="container mx-auto px-2">
         <Outlet />
       </main>
     </div>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ export default function NotFoundPage() {
   <div>
     <div>nothing to see here</div>
 
-    <Link to='/' replace className='p-2 border-2 border-black rounded-md inline-block hover:bg-black hover:text-white'>
+    <Link to="/" replace className="p-2 border-2 border-black rounded-md inline-block hover:bg-black hover:text-white">
       Go home
     </Link>
   </div>
