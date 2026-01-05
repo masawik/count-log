f3c0700f50f3cfae4c3fde8ce2910b6c29524640
Aleksandr <rocheval.dev@gmail.com>
2026-01-05 11:15:01 +0300

    build: add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
-  plugins: [ tailwindcss(), reactRouter(), tsconfigPaths() ],
-})
+export default defineConfig(({ isSsrBuild }) => ({
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+
+    (!isSsrBuild &&
+    visualizer({
+      filename: 'build/client_stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    })),
+  ],
+}))
